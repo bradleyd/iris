31438c46d4fc78bbea2d387a077db43a443426b2
Bradley Smith <bradleydsmith@gmail.com>
2017-09-10 10:56:08 -0600

    notify if gen_smtp pid goes away

--- lib/iris/listener.ex
+++ lib/iris/listener.ex
@@ -1,5 +1,6 @@
 defmodule Iris.Listener do
   use GenServer
+  require Logger
 
   @config Application.get_all_env(:iris)
 
@@ -8,11 +9,22 @@ defmodule Iris.Listener do
   end
 
   def init(_args) do
+    Process.flag(:trap_exit, true)
     server_options = @config |> Keyword.get(:listener)
     server = @config |> Keyword.get(:server)
     {:ok, pid} = :gen_smtp_server.start(server, server_options)
     ref = Process.monitor(pid)
     {:ok, ref}
   end
+
+  def handle_info({:DOWN, _ref, :process, pid, reason}, state) do
+    Logger.error("SMTP server pid #{inspect(pid)} has gone down because #{inspect(reason)}")
+    {:stop, state}
+  end
+  def handle_info({:EXIT, pid, reason}, state) do
+    Logger.error("SMTP server pid #{inspect(pid)} has exited because #{inspect(reason)}")
+    {:stop, state}
+  end
+
 
 end
